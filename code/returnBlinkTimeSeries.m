313b19790d509c16551a4377d8c85e603c279aec
gkaguirre <gkaguirre@me.com>
2023-07-26 11:02:15 -0400

    Starting on this

---
+++ code/returnBlinkTimeSeries.m
@@ -0,0 +1,255 @@
+function [blinkVector,temporalSupport, nTrials, blinkVectorRaw, trialIndices, blinkVectorBoots, bootSam] = returnBlinkTimeSeries( subjectID,sessionID,scanNumbers,ipsiOrContra,discardFirstTrialFlag,discardSquintScansFlag,nBootResamples,minValidIpsiBlinksPerAcq,minValidAcq,nSamplesBeforeStim,nSamplesAfterStim,deltaT )
+% Loads I-Files and conducts an analysis of time series data
+%
+% Syntax:
+%   [blinkVector,temporalSupport] = returnBlinkTimeSeries( subjectID, targetPSI )
+%
+% Description:
+%   Briana: Describe how the iFiles are structured and how we load them
+%
+%   The videos are recorded with a 300 Hz camera, implying a deltaT of 3.33
+%   msecs. This is supported by observing that the first 31 observations
+%   correspond to 0-100 msecs, thus 100 msecs / 30 intervals = 3.33 msecs.
+%
+% Inputs:
+%   subjectID             - Scalar. 5 digit integer that identifies the
+%                           subject.
+%   targetPSI             - Scalar. One of the valid PSI targets:
+%                               {3.5, 7.5, 15, 30, 60}
+%                           If set empty, all PSI levels are used
+%   sessionID             - Scalar. Valid values are:
+%                               {1, 2}
+%                           If set empty, both sessions are used
+%
+% Outputs:
+%   blinkVector           - Vector
+%   temporalSupport       - Vector
+%   nTrials               - Scalar
+%   blinkVectorRaw        - nTrials x time matrix of raw responses
+%
+% Examples:
+%{
+    scanNumbers = [2, 10, 11, 18, 22];
+    subjectID = 'BLNK_0001';
+    sessionID = '2023-07-19';
+    [blinkVector,temporalSupport] = returnBlinkTimeSeries( subjectID, sessionID, scanNumbers, 'ipsi' );
+    figure
+    plot(temporalSupport,blinkVector,'-r')
+%}
+%{
+    % Bootstrap resample across all acquisitions at one PSI
+    targetPSI = 30;
+    subjectID = 15513;
+    discardFirstTrialFlag = true;
+    discardSquintScansFlag = true;
+    nBootResamples = 1000;
+    [blinkVector,temporalSupport,~,~,~,blinkVectorBoots,bootSam] = ...
+        returnBlinkTimeSeries( subjectID, targetPSI, [], 'ipsi', discardFirstTrialFlag, discardSquintScansFlag, nBootResamples );
+%}
+
+
+arguments
+    subjectID
+    sessionID
+    scanNumbers
+    ipsiOrContra = 'ipsi';
+    discardFirstTrialFlag = false;
+    discardSquintScansFlag = false;
+    nBootResamples (1,1) {mustBeNumeric} = 0;
+    minValidIpsiBlinksPerAcq (1,1) {mustBeNumeric} = 0;
+    minValidAcq = 0;
+    nSamplesBeforeStim = 10;
+    nSamplesAfterStim = 150;
+    deltaT = 3.333;
+end
+
+    patientID = 16813;
+
+% Set a counter to return
+nTrials = 0;
+
+% Initialize vectors for return
+blinkVectorRaw = [];
+trialIndices = [];
+blinkVectorBoots = [];
+
+% Define the location of the i-files
+dataDirPath = fullfile( ...
+    getpref('BLNK_2023_Expt','analysisDir'), ...
+    subjectID, sessionID );
+
+% Define the location of the summary spreadsheet that we will use to
+% determine if a given trial is valid
+spreadsheet ='BUPenn_A FILE_A-mod_2023-07-21.xls';
+
+% Turn off a warning during readtable
+warnState = warning();
+warning('off','MATLAB:table:ModifiedAndSavedVarnames');
+
+% Read the table
+T = readtable(fullfile(dataDirPath,spreadsheet));
+
+% Restore the warning state
+warning(warnState);
+
+% Find the scans for this subject
+scanTable = T(ismember(T.PatientID,patientID),:);
+
+% Store the scan dates
+scanDates = unique(scanTable.ScanDate);
+
+% Now cull the table to remove invalid scans
+if discardSquintScansFlag
+    scanTable = scanTable(ismember(scanTable.notSquint,'TRUE'),:);
+    scanTable = scanTable(scanTable.numIpsi>=minValidIpsiBlinksPerAcq,:);
+end
+
+% Filter the table for just the requested scan numbers
+scanTable = scanTable(ismember(scanTable.ScanNumber,scanNumbers),:);
+
+% Filter out "invalid" blinks (per BlinkCNS processing) if the setting
+% minValidIpsiBlinksPerAcq is greater than zero
+if minValidIpsiBlinksPerAcq > 0
+    scanTable = scanTable(ismember(scanTable.Valid,'TRUE'),:);
+end
+
+% Check if we have an empty scanTable
+if isempty(scanTable)
+    blinkVector = nan(1,nSamplesBeforeStim+nSamplesAfterStim+1);
+    temporalSupport = nan(1,nSamplesBeforeStim+nSamplesAfterStim+1);
+    return
+end
+
+% Check if we have too few acquisitions
+if size(scanTable,1) < minValidAcq
+    blinkVector = nan(1,nSamplesBeforeStim+nSamplesAfterStim+1);
+    temporalSupport = nan(1,nSamplesBeforeStim+nSamplesAfterStim+1);
+    return
+end
+
+% Turn off a warning during readtable
+warnState = warning();
+warning('off','MATLAB:table:ModifiedAndSavedVarnames');
+
+% Loop over the acquisitions
+for ii = 1:size(scanTable,1)
+
+    % In principle, the iFile name should be fully defined by the
+    % information in the data table provided by Andy from BlinkTBI. In
+    % practice, the scanID and scanNumber in the filenames do not agree
+    % with that in the table. It seems that the scanNumber value from the
+    % table can be ignored, and the scanID used as the unique identifier of
+    % the file. So, we find the matching scanID file, and check to make
+    % sure it is the only match
+    iFileName = ['l-file_' num2str(scanTable.subjectID(ii)) '_' num2str(scanTable.scanID(ii)) '_' '*' '.csv'];
+    fullFilePath = fullfile(dataDirPath,'data',dataSubdir,'iFiles',num2str(subjectID),iFileName);
+    fileList = dir(fullFilePath);
+    if length(fileList)>1
+        error('Too many files');
+    else
+        fullFilePath = fullfile(fileList.folder,fileList.name);
+    end
+
+    % Load the iFile into a table
+    T = readtable(fullFilePath);
+
+    % find stimulus arrivals
+    stimVarName = T.Properties.VariableNames{2};
+    rights = find(strcmp('MC-OD',T.(stimVarName)(:,1)));
+    lefts = find(strcmp('MC-OS',T.(stimVarName)(:,1)));
+    all = sort(cat(1,rights,lefts));
+
+    % get times
+    starts = all - nSamplesBeforeStim;
+    ends = all + nSamplesAfterStim;
+
+    % Issue a warning if there are fewer than 8 trials
+    if length(starts)<8
+        fprintf(['Only %d trials: ' fullfile(num2str(subjectID),iFileName) '\n'],length(starts));
+    end
+
+    % get means across trials
+    pos = nan(length(starts),nSamplesBeforeStim+nSamplesAfterStim+1);
+
+    for jj = 1:length(starts)
+
+        % Handle the edge case of the time-series starting after the
+        % desired "numBefore" window
+        offset = max([1 -starts(jj)+2]);
+
+        % Handle the laterality of the stimulus and the choice of returning
+        % the ipsi or contra response
+        if ismember(all(jj),rights)
+            switch ipsiOrContra
+                case 'ipsi'
+                    columnIdx = 3;
+                case 'contra'
+                    columnIdx = 4;
+                case 'both'
+                    columnIdx = [3, 4];
+            end
+        else
+            switch ipsiOrContra
+                case 'ipsi'
+                    columnIdx = 4;
+                case 'contra'
+                    columnIdx = 3;
+                case 'both'
+                    columnIdx = [3, 4];
+            end
+        end
+
+        % Get this timeseries
+        temp = table2array(T(max([1 starts(jj)]):ends(jj),columnIdx));
+
+        % Add it to the matrix
+        pos(jj,offset:end) = mean(temp,2)';
+
+        % Store the trial index
+        trialIndices = [trialIndices jj];
+
+        % Increment the trial count
+        nTrials = nTrials + 1;
+
+    end
+
+    % Discard the first trial if requested
+    if discardFirstTrialFlag
+        pos = pos(2:end,:);
+    end
+
+    % center pre-stimulus around zero
+    posAvg = nanmean(pos);
+    posAvgPreStim = mean(posAvg(1:nSamplesBeforeStim));
+    posAvg = posAvg - posAvgPreStim;
+
+    % Store the response
+    respByAcq(ii,:) = posAvg;
+
+    % Create a concatenated, raw vector of responses
+    blinkVectorRaw = [blinkVectorRaw; pos-mean(pos(:,1:nSamplesBeforeStim),2)];
+
+end
+
+% Restore the warning state
+warning(warnState);
+
+% Get the mean deltaT and assemble the temporal support
+temporalSupport = -nSamplesBeforeStim*deltaT:deltaT:nSamplesAfterStim*deltaT;
+
+% Conduct a resampling with replacement of the vector if requested. We
+% create an anonymous function for the mean to force action over the first
+% dimension in case there is just one acquisition. We also reset the random
+% seed every time we reach this point so that we use the same bootSam every
+% time, thus linking the resampling across acquisitions and sides (ipsi /
+% contra)
+if nBootResamples>0
+    rng default;
+    meanFunc = @(x) mean(x,1);
+    [blinkVectorBoots, bootSam] = bootstrp(nBootResamples,meanFunc,respByAcq);
+end
+
+% Return the blinkVector
+blinkVector = mean(respByAcq,1);
+
+end
---
+++ config/BLNK_2023_ExptLocalHookTemplate.m
@@ -0,0 +1,57 @@
+function BLNK_2023_ExptLocalHook
+%  BLNK_2023_ExptLocalHook
+%
+% For use with the ToolboxToolbox.
+%
+% As part of the setup process, ToolboxToolbox will copy this file to your
+% ToolboxToolbox localToolboxHooks directory (minus the "Template" suffix).
+% The defalt location for this would be
+%   ~/localToolboxHooks/BLNK_2023_ExptLocalHook.m
+%
+% Each time you run tbUseProject('BLNK_2023_Expt'), ToolboxToolbox will
+% execute your local copy of this file to do setup for BLNK_2023_Expt.
+%
+% You should edit your local copy with values that are correct for your
+% local machine, for example the output directory location.
+%
+
+
+projectName = 'BLNK_2023_Expt';
+
+%% Delete any old prefs
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
+% Define a save location for plots
+if ismac
+    [~, userName] = system('whoami');
+    userName = strtrim(userName);
+elseif isunix
+    userName = getenv('USER');
+elseif ispc
+    userName = getenv('username');
+else
+    disp('What are you using?')
+end
+
+
+% DropBox
+dropboxBaseDir = ...
+    fullfile('/Users', userName, ...
+    'Dropbox (Aguirre-Brainard Lab)');
+
+% Path to paper directory
+switch userName
+    case 'aguirre'
+        analysisDir = fullfile(dropboxBaseDir,'BLNK_analysis','expt01_summer2023');
+        dataDir = fullfile(dropboxBaseDir,'BLNK_data','expt01_summer2023');
+    otherwise
+        analysisDir = fullfile(dropboxBaseDir,'BLNK_analysis','expt01_summer2023');
+        dataDir = fullfile(dropboxBaseDir,'BLNK_data','expt01_summer2023');
+end
+
+setpref(projectName,'analysisDir',analysisDir);
+setpref(projectName,'dataDir',dataDir);
+
+
