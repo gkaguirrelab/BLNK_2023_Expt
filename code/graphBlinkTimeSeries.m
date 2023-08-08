7efec17b953c2477cab073733688a649e1318b1e
Gerdin Falconi <gerdin.falconi@pennmedicine.upenn.edu>
2023-08-08 17:40:59 -0400

    Added graph legend and improved code

--- code/graphBlinkTimeSeries.m
+++ code/graphBlinkTimeSeries.m
@@ -20,27 +20,33 @@ function graphBlinkTimeSeries(subjectID, sessionID)
 %}
     
     % Pressure levels to plot
-    scanNumbers(1,:) = [4, 7, 13, 20, 21]; % 40 PSI
-    scanNumbers(2,:) = [2, 10, 11, 18, 22]; % 20 PSI
+    scanNumbers(1,:) = [1, 9, 14, 19, 25]; % 0 PSI
+    scanNumbers(2,:) = [5, 6, 12, 17, 24]; % 5 PSI
     scanNumbers(3,:) = [3, 8, 15, 16, 23]; % 10 PSI
-    scanNumbers(4,:) = [5, 6, 12, 17, 24]; % 5 PSI
-    scanNumbers(5,:) = [1, 9, 14, 19, 25]; % 0 PSI
+    scanNumbers(4,:) = [2, 10, 11, 18, 22]; % 20 PSI
+    scanNumbers(5,:) = [4, 7, 13, 20, 21]; % 40 PSI
     
-    colors = ['r', 'b', 'g', 'k', 'm'];
+    % Set up graph figure
     figure('Name',sprintf('%s Time Series', subjectID));
+    xlabel('Time (msecs)');
+    ylabel('Lid Position (pixels)');
+    title(sprintf('%s Time Series', subjectID), 'Interpreter', 'none');
+    
+    % Make graph easier to visualize
+    colors = ['m', 'k', 'g', 'b', 'r'];
+    legendNames = ["0 PSI", "5 PSI", "10 PSI", "20 PSI", "40 PSI"];
     
     for ii = 1:size(scanNumbers,1)
         [blinkVector,blinkVectorSEM,temporalSupport] = returnBlinkTimeSeries(subjectID, sessionID, scanNumbers(ii,:), 'ipsi' );
         
         % Graph
-        pHandle = patch([temporalSupport,fliplr(temporalSupport)],[blinkVector+blinkVectorSEM,fliplr(blinkVector-blinkVectorSEM)],colors(ii));
+        pHandle = patch([temporalSupport,fliplr(temporalSupport)],[blinkVector+blinkVectorSEM,fliplr(blinkVector-blinkVectorSEM)],colors(ii), 'HandleVisibility','off');
         pHandle.FaceAlpha = 0.1; pHandle.LineStyle = 'none';
         hold on;
-        plot(temporalSupport,blinkVector,colors(ii),'LineWidth',2);
-        xlabel('Time (msecs)');
-        ylabel('Lid Position (pixels)');
-        title(sprintf('%s Time Series', subjectID), 'Interpreter', 'none');
+        plot(temporalSupport,blinkVector,colors(ii),'LineWidth',2, 'DisplayName',legendNames(ii));
     end
+
+    legend('Location','west')
     
     % Automatically saves figure to Gerdin's Dropbox directory
     %savefig(sprintf('/Users/gerdinfalconi/Aguirre-Brainard Lab Dropbox/Gerdin Falconi/BLNK_analysis/expt01_summer2023/%s/%s/%s Time Series', subjectID, sessionID, subjectID));
